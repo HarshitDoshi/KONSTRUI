8fb2519e8775b75e4e8d76087bb1980d33b96b76
Harshit <5103046+HarshitDoshi@users.noreply.github.com>
2021-11-26 16:14:45 +0530

    Add system information script

---
+++ Scripts/system-information.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Operating System:       $(uname -o)"
+echo "Kernel Information:"
+echo "  Name:                 $(uname -s)"
+echo "  Release:              $(uname -r)"
+echo "  Version:              $(uname -v)"
+echo "Hostname:               $(uname -n)"
+echo "Machine Hardware Name:  $(uname -m)"
+echo "Processor Type:         $(uname -m)"
+echo "Hardware Platform:      $(uname -i)"
